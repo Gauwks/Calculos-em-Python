a03185cb5e76a4af23a112946fce42480db6cf04
Maria Clara <maknae.games7@gmail.com>
2025-02-24 22:23:36 -0300

    Cálculos em Python

---
+++ calculos.py
@@ -0,0 +1,17 @@
+print('Bem-vindo(a) ao Super Calculador 6000 em Python!')
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite um número: "))
+
+print('Operações Básicas: \n')
+input("\ta - Adição")
+input("\ts - Subtração")
+input("\tm - Multiplicação")
+input("\td - Divisão \n")
+
+print('Operações avançadas: ')
+input("\tM - Média")
+input("\tA - Área de um Retângulo com o primeiro número como base")
+input("\tA2 - Área de um Retângulo com o segundo número como base")
+input("\tAT - Área de um Triângulo com o primeiro número como base")
+input("\tA - Área de um Triângulo com o segundo número como base")
